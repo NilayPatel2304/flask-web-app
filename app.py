ac0f89625ffc40c213bdaec5895f928ac97cc325
NilayPatel2304 <nilaytpatel76@gmail.com>
2024-12-05 21:51:07 -0500

    Fix import error in app.py (use unquote from werkzeug.urls)

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug.urls import unquote  # Use unquote instead of url_quote
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    # Example of using unquote (if needed in your app)
+    query_param = request.args.get('some_param', '')
+    decoded_param = unquote(query_param)
+    
+    return render_template('index.html', decoded_param=decoded_param)
 
 if __name__ == '__main__':
     app.run(debug=True)
